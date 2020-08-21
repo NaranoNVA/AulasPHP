56081ad8dde752c17cfe794dc1d146f141ee0ad2
Nicolas Alberto <darkathnva@gmail.com>
2020-08-20 22:11:16 -0300

    Aula com exemplo de operadores rodando no servidor web

---
+++ operadores/index.php
@@ -0,0 +1,24 @@
+<?php
+//Comentario em linha
+
+/*
+Comentario de bloco
+*/
+
+$saldo = 34.5;
+
+//Aspas simples apenas string pura
+echo '<br><br><br>Seu saldo é $saldo ';
+
+//Aspas duplas aceitam strig expecial
+echo "<br><br><br>Seu saldo é $saldo ";
+
+
+$saldo  = $saldo - 10;//Operador de Subtração
+echo "<br><br><br>Agora seu saldo é $saldo ";
+
+$saldo  -= 10;//Operador de Subtração
+echo "<br><br><br>Agora seu saldo é $saldo ";
+
+$saldo  *= 1000;//Operador de Multiplicação
+echo "<br><br><br>Agora seu saldo é $saldo ";
